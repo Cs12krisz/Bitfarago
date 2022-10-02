ee2ff159c60042e0c7f805ba1600c887d128a50b
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-10-02 22:52:14 +0200

    Add files via upload

--- teszt.js
+++ teszt.js
@@ -1,98 +1,142 @@
-let filmek = [ "Cím: Csillagok között <br> Megjelenési év: 2014",   //0
-               "Cím: Mentőexpedíció <br> Megjelenési év: 2015",     //1
-               "Cím: A szem tükrében <br> Megjelenési év: 2015",    //2
-               "Cím: Teljesen idegenek <br>  Megjelenési év: 2016", //3
-               "Cím: Csillagainkban a hiba <br> Megjelenési év: 2014",//4 
-               "Cím: A sötét lovag: Felemelkedés <br> Megjelenési év: 2012",//5 
-               "Cím: Mission: Impossible - Utóhatás <br> Megjelenési év: 2018",//6 
-               "Cím: Egy nap <br> Megjelenési év: 2011", //7
-               "Cím: Ezer szó <br> Megjelenési év: 2012 ",//8
-               "Cím: Másnaposok <br> Megjelenési év: 2009", //9
-               "Cím: Viharsziget <br> Megjelenési év: 2010",//10
-               "Cím: A segítség <br> Megjelenési év: 2011",//11
-               "Cím: Családi üzelmek <br> Megjelenési év: 2013",//12 
-               "Cím: Low Tide <br> Megjelenési év: 2019", //13
-               "Cím: Joker <br> megjelenési év: 2019" ] //14
-            //`dőlt idézőjelen belül szöveg, ha valtozót akarunk hívni csak ${valtozo} különítjük dőlt idéző jelen belül`
-            
-            document.getElementById("valogat").onclick = function(){
-                if(document.getElementById("sci-fi").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[0]} <br><br> ${filmek[1]}  <br><br> ${filmek[2]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("drama").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[0]} <br><br> ${filmek[1]}   <br><br> ${filmek[2]} <br><br> ${filmek[4]} 
-                    <br><br> ${filmek[3]} <br><br> ${filmek[14]} <br><br> ${filmek[11]}  <br><br> ${filmek[13]} <br><br> ${filmek[8]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("akcio").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[6]} <br><br> ${filmek[5]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("kaland").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[0]} <br><br> ${filmek[1]} <br><br> ${filmek[5]} <br><br> ${filmek[6]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("thriller").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[6]} <br><br> ${filmek[10]} <br><br> ${filmek[13]} <br><br> ${filmek[14]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("vigjatek").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[12]} <br><br> ${filmek[8]} <br><br> ${filmek[3]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("romantika").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[7]} <br><br> ${filmek[3]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("krimi").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[14]} <br><br> ${filmek[12]}`
-                }else{
-                    document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                }
-                if(document.getElementById("sci-fi").checked){
-                   if(document.getElementById("drama").checked){
-                    document.getElementById("filmek").innerHTML = `${filmek[0]} <br><br> ${filmek[1]}   <br><br> ${filmek[2]}`
-                    }else{
-                        document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                    }
-                }
-                if(document.getElementById("sci-fi").checked){
-                    if(document.getElementById("akcio").checked){
-                        alert("Sajnos nincs ilyen fajta filmünk MÉG. Válassz valami mást.")
-                        document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                    }
-                }
-                if(document.getElementById("sci-fi").checked){
-                    if(document.getElementById("kaland").checked){
-                        document.getElementById("filmek").innerHTML = `${filmek[0]} <br><br> ${filmek[1]}`
-                    }
-                }else{
-                    document.getElementById("filmek").innerHTML = ""
-                }
-                if(document.getElementById("sci-fi").checked){
-                    if(document.getElementById("thriller").checked){
-                        alert("Sajnos nincs ilyen fajta filmünk MÉG.")
-                        document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                    }
-                }
-                if(document.getElementById("sci-fi").checked){
-                    if(document.getElementById("vigjatek").checked){
-                        alert("Sajnos nincs ilyen fajta filmünk MÉG.")
-                        document.getElementById("filmek").innerHTML = "Válassz ki előtte egy műfajt vagy többet!"
-                    }
-                }
-                if(document.getElementById("sci-fi").checked){
-                    if(document.getElementById("romantika").checked){
-                        document.getElementById("filmek").innerHTML = `${filmek[2]}`
-                    }
-                }
+let filmek = [ "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>",
+               "Cím: Mentőexpedíció <br> Megjelenési év: 2015 <br><br>",
+               "Cím: A szem tükrében <br> Megjelenési év: 2015 <br><br>",
+               "Cím: Teljesen idegenek <br>  Megjelenési év: 2016 <br><br>",
+               "Cím: Csillagainkban a hiba <br> Megjelenési év: 2014 <br><br>",
+               "Cím: A sötét lovag: Felemelkedés <br> Megjelenési év: 2012 <br><br>",
+               "Cím: Mission: Impossible - Utóhatás <br> Megjelenési év: 2018 <br><br>",
+               "Cím: Egy nap <br> Megjelenési év: 2011 <br><br>",
+               "Cím: Ezer szó <br> Megjelenési év: 2012 <br><br>",
+               "Cím: Másnaposok <br> Megjelenési év: 2009 <br><br>",
+               "Cím: Viharsziget <br> Megjelenési év: 2010 <br><br>",
+               "Cím: A segítség <br> Megjelenési év: 2011 <br><br>",
+               "Cím: Családi üzelmek <br> Megjelenési év: 2013 <br><br>",
+               "Cím: Low Tide <br> Megjelenési év: 2019 <br><br> ",
+               "Cím: Joker <br> megjelenési év: 2019 <br><br>" ]
+
+
+
+let proba ={kat1: "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>"}
+let kesz = [proba]
+document.getElementById("valogat").onclick = function valogat(){
+
+
+    const scifi =     document.getElementById("sci-fi").checked
+    const akcio =     document.getElementById("akcio").checked
+    const romantika = document.getElementById("romantika").checked
+    const thriller =  document.getElementById("thriller").checked
+    const kaland =    document.getElementById("kaland").checked
+    const drama =     document.getElementById("drama").checked
+    const vigjatek =  document.getElementById("vigjatek").checked
+    const krimi =     document.getElementById("krimi").checked
+
+    const fajtak = [scifi, akcio,romantika,thriller,kaland,drama,vigjatek,krimi]
+    kiiro = ""
+    kereses = fajtak
+    
+        if (fajtak[0] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[0]} ${filmek[1]} ${filmek[2]}`;
+        }
+        if(fajtak[0] && fajtak[5] == true){
+            document.getElementById("filmek").innerHTML =  `${filmek[0]} ${filmek[1]} ${filmek[2]}`;
+        }
+        if(fajtak[0] && fajtak[2] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[2]}`;
+        }
+        if(fajtak[0] && fajtak[4] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[0]} ${filmek[1]}`;
+        }
+        if(fajtak[1] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[6]}  ${filmek[5]}`;
+        }
+        if(fajtak[1] && fajtak[3] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[7]}`;
+        }
+        if(fajtak[1] && fajtak[4] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[5]} ${filmek[6]}`;
+        }
+        if(fajtak[2] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[7]} ${filmek[2]} ${filmek[4]}`;
+        }
+        if(fajtak[2] && fajtak[5] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[4]} ${filmek[2]}`;
+        }
+        if(fajtak[2] && fajtak[0] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[2]}`;
+        }
+        if(fajtak[3] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[6]}  ${filmek[10]} ${filmek[13]} ${filmek[14]}`;
+        }
+        if(fajtak[3] && fajtak[5] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[14]} ${filmek[13]}`;
+        }
+        if(fajtak[3] && fajtak[7] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[14]}`;
+        }
+        if(fajtak[3] && fajtak[4] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[6]}`;
+        }
+        if(fajtak[3] && fajtak[1] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[7]}`;
+        }
+        if(fajtak[4] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[0]}  ${filmek[1]}  ${filmek[5]} ${filmek[6]}`;
         } 
-            
+        if(fajtak[4] && fajtak[5] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[1]} ${filmek[0]}`;
+        }
+        if(fajtak[5] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[0]} ${filmek[1]} ${filmek[2]} ${filmek[4]} ${filmek[3]} ${filmek[14]} ${filmek[11]} ${filmek[13]} ${filmek[8]}`;
+        }
+        if(fajtak[5] && fajtak[7] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[14]}`;
+        }
+        if(fajtak[5] && fajtak[6] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[9]} ${filmek[3]}`;
+        }
+        if(fajtak[6] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[3]} ${filmek[9]} ${filmek[12]}`;
+        }
+        if(fajtak[6] && fajtak[7] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[12]}`;
+        }
+        if(fajtak[0] && fajtak[4] && fajtak[5] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[0]} ${filmek[1]}`;
+        }
+        if(fajtak[1] && fajtak[4] && fajtak[3] == true){
+            document.getElementById("filmek").innerHTML = `${filmek[6]}`;
+        }
+        if(fajtak[0] && fajtak[1] && fajtak[2] && fajtak[3] && fajtak[4] && fajtak[5] && fajtak[6] && fajtak[7] == true){
+            document.getElementById("filmek").innerHTML = "Sajnos, nincs ilyen film.";
+        }
+        if(fajtak[0] && fajtak[1] && fajtak[2] && fajtak[3] && fajtak[4] && fajtak[5] && fajtak[6] == true){
+            document.getElementById("filmek").innerHTML = "Sajnos, nincs ilyen film.";
+        }
+   
+        for(let x in proba){
+            if(fajtak[0] == true){
+                kiiro = kesz[x].kat1
+                document.getElementById("filmek").innerHTML = kiiro
+            }
+        }
+/*
+const film1 = {title:"Cápa 1", cat1:"horror", cat2:"kaland", picture:"kep1.jpg"};
+const film2 = {title:"Superman", cat1:"kaland", cat2:"vígjáték",picture:"kep2.jpg"};
+const film3 = {title:"Forest Gump", cat1:"dráma", cat2:"vígjáték",picture:"kep3.jpg"};
+const film4 = {title:"Top Gun", cat1:"dráma", cat2:"kaland",picture:"kep4.jpg"};
+const list=[film1,film2,film3, film4]
+kiirkod="";
+keresettkat1="kaland"
+for (let x in list)
+	{
+		if (list[x].cat1==keresettkat1 || list[x].cat2==keresettkat1){
+		
+		
+		
+		kiirkod+="<p>"+list[x].title+" <img src='"+list[x].picture+"' /></p>";
+	}
+	}
+	document.getElementById("filmek").innerHTML = kiirkod;
+    */
+
+}
