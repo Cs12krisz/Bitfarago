6b3f2f09dbc919ae4466ce8fcc093942797cde3e
Cs12krisz <90339656+Cs12krisz@users.noreply.github.com>
2022-10-03 14:53:12 +0200

    Add files via upload

--- teszt.js
+++ teszt.js
@@ -1,3 +1,4 @@
+/*
 let filmek = [ "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>",
                "Cím: Mentőexpedíció <br> Megjelenési év: 2015 <br><br>",
                "Cím: A szem tükrében <br> Megjelenési év: 2015 <br><br>",
@@ -13,29 +14,57 @@ let filmek = [ "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>",
                "Cím: Családi üzelmek <br> Megjelenési év: 2013 <br><br>",
                "Cím: Low Tide <br> Megjelenési év: 2019 <br><br> ",
                "Cím: Joker <br> megjelenési év: 2019 <br><br>" ]
+*/
 
 
+let film1 ={cim: "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>", kat1:"sci-fi", kat2:"dráma", kat3:"kaland"}
+let film2 ={cim: "Cím: Mentőexpedíció <br> Megjelenési év: 2015 <br><br>", kat1:"sci-fi", kat2:"dráma", kat3: "kaland"}
+let film3 ={cim: "Cím: A szem tükrében <br> Megjelenési év: 2015 <br><br>", kat1:"sci-fi", kat2:"dráma", kat3: "romantika"}
+let film4 ={cim: "Cím: Teljesen idegenek <br>  Megjelenési év: 2016 <br><br>", kat1: "vígjáték", kat2:"dráma" }
+let film5 ={cim: "Cím: Csillagainkban a hiba <br> Megjelenési év: 2014 <br><br>", kat1:"romantika",kat2:"dráma" }
+let film6 ={cim: "Cím: A sötét lovag: Felemelkedés <br> Megjelenési év: 2012 <br><br>", kat1:"akció",kat2:"kaland"}
+let film7 ={cim: "Cím: Mission: Impossible - Utóhatás <br> Megjelenési év: 2018 <br><br>", kat1:"akció", kat2:"thriller", kat3:"kaland"}
+let film8 ={cim: "Cím: Egy nap <br> Megjelenési év: 2011 <br><br>", kat1:"romantika", kat2:"dráma"}
+let film9 ={cim: "Cím: Ezer szó <br> Megjelenési év: 2012 <br><br>", kat1:"vígjáték", kat2:"dráma"}
+let film10 ={cim: "Cím: Másnaposok <br> Megjelenési év: 2009 <br><br>", kat1:"vígjáték"}
+let film11 ={cim: "Cím: Viharsziget <br> Megjelenési év: 2010 <br><br>", kat1:"thriller"}
+let film12 ={cim: "Cím: A segítség <br> Megjelenési év: 2011 <br><br>", kat1:"dráma"}
+let film13 ={cim: "Cím: Családi üzelmek <br> Megjelenési év: 2013 <br><br>", kat1:"vígjáték", kat2:"krimi"}
+let film14 ={cim: "Cím: Low Tide <br> Megjelenési év: 2019 <br><br> ", kat1:"thriller", kat2:"dráma"}
+let film15 ={cim: "Cím: Joker <br> megjelenési év: 2019 <br><br>", kat1:"thriller", kat2:"dráma", kat3:"krimi"}
 
-let proba ={kat1: "Cím: Csillagok között <br> Megjelenési év: 2014 <br><br>"}
-let kesz = [proba]
+let filmek= [film1,film2,film3,film4,film5,film6,film7,film8,film9,film10,film11,film12, film13, film14, film15]
 document.getElementById("valogat").onclick = function valogat(){
 
 
-    const scifi =     document.getElementById("sci-fi").checked
-    const akcio =     document.getElementById("akcio").checked
-    const romantika = document.getElementById("romantika").checked
-    const thriller =  document.getElementById("thriller").checked
-    const kaland =    document.getElementById("kaland").checked
-    const drama =     document.getElementById("drama").checked
-    const vigjatek =  document.getElementById("vigjatek").checked
-    const krimi =     document.getElementById("krimi").checked
+    const scifi1 =     document.getElementById("sci-fi").checked
+    const akcio1 =     document.getElementById("akcio").checked
+    const romantika1 = document.getElementById("romantika").checked
+    const thriller1 =  document.getElementById("thriller").checked
+    const kaland1 =    document.getElementById("kaland").checked
+    const drama1 =     document.getElementById("drama").checked
+    const vigjatek1 =  document.getElementById("vigjatek").checked
+    const krimi1 =     document.getElementById("krimi").checked
 
-    const fajtak = [scifi, akcio,romantika,thriller,kaland,drama,vigjatek,krimi]
-    kiiro = ""
-    kereses = fajtak
-    
+    const fajtak1 = [scifi1, akcio1, romantika1,thriller1,kaland1,drama1,vigjatek1,krimi1]
+   
+    const scifi2 =     document.getElementById("sci-fi")
+    const akcio2 =     document.getElementById("akcio")
+    const romantika2 = document.getElementById("romantika")
+    const thriller2 =  document.getElementById("thriller")
+    const kaland2 =    document.getElementById("kaland")
+    const drama2 =     document.getElementById("drama")
+    const vigjatek2 =  document.getElementById("vigjatek")
+    const krimi2 =     document.getElementById("krimi")
+   const fajtak2 = [scifi2, akcio2, romantika2,thriller2,kaland2,drama2,vigjatek2,krimi2]
+
+   kereses = fajtak2
+kiiro = ""
+    /*
         if (fajtak[0] == true){
             document.getElementById("filmek").innerHTML = `${filmek[0]} ${filmek[1]} ${filmek[2]}`;
+        }else if(fajtak){
+
         }
         if(fajtak[0] && fajtak[5] == true){
             document.getElementById("filmek").innerHTML =  `${filmek[0]} ${filmek[1]} ${filmek[2]}`;
@@ -112,13 +141,16 @@ document.getElementById("valogat").onclick = function valogat(){
         if(fajtak[0] && fajtak[1] && fajtak[2] && fajtak[3] && fajtak[4] && fajtak[5] && fajtak[6] == true){
             document.getElementById("filmek").innerHTML = "Sajnos, nincs ilyen film.";
         }
-   
-        for(let x in proba){
-            if(fajtak[0] == true){
-                kiiro = kesz[x].kat1
+   */
+        for(let x in filmek){
+            if(fajtak1[0] == true){
+                console.log(kereses)
+                if(kereses == filmek[x].kat1 || filmek[x].kat2 || filmek[x].kat3){
+                kiiro = filmek[x].cim
                 document.getElementById("filmek").innerHTML = kiiro
             }
         }
+    }
 /*
 const film1 = {title:"Cápa 1", cat1:"horror", cat2:"kaland", picture:"kep1.jpg"};
 const film2 = {title:"Superman", cat1:"kaland", cat2:"vígjáték",picture:"kep2.jpg"};
